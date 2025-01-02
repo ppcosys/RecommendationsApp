386938f484321920ff2cccba220c77b6d802fd69
Peter <ppcosys@gmail.com>
2025-01-02 23:41:05 +0100

    Added Recommendation class and DataContext.

---
+++ Domain/Recommendation.cs
@@ -0,0 +1,14 @@
+namespace Domain
+{
+    public class Recommendation
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Link { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }        
+        public string Country {get; set;}
+        public string Place {get; set;}
+    }
+}
