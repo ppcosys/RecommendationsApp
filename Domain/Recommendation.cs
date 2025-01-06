461cecae1b7c4ee3c097edba22eeb857d68ea9c3
Peter <ppcosys@gmail.com>
2025-01-06 13:59:29 +0100

    Update of Recommendation Class and Database created.

--- Domain/Recommendation.cs
+++ Domain/Recommendation.cs
@@ -8,7 +8,8 @@ namespace Domain
         public string Link { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }        
-        public string Country {get; set;}
-        public string Place {get; set;}
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Place { get; set; }
     }
 }
