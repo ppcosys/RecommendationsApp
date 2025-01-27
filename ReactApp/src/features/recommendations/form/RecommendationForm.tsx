28d975ec13a624e3e8a2ac036db339e40d4bb92a
Peter <ppcosys@gmail.com>
2025-01-27 15:00:37 +0100

    Enabling editing of existing Recommendations in the frontend application.

--- ReactApp/src/features/recommendations/form/RecommendationForm.tsx
+++ ReactApp/src/features/recommendations/form/RecommendationForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { Button, Form, FormInput, FormTextArea, Segment } from "semantic-ui-react";
 import { Recommendation } from "../../../app/models/recommendation";
 
@@ -8,18 +8,42 @@ interface Props {
 }
 
 
-export default function RecommendationForm({recommendation, closeForm}: Props){
+export default function RecommendationForm({recommendation: selectedRecommendation, closeForm}: Props){
+    
+    const initialState = selectedRecommendation ?? {
+        id: '',
+        title: '',
+        category: '',
+        link: '',
+        date: '',
+        description: '',
+        country: '',
+        city: '',
+        place: ''
+    }
+    
+    const [recommendation, setRecommendation] = useState(initialState);
+
+    function handleSubmit(){
+        console.log(recommendation);
+    }
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const {name, value} = event.target;
+        setRecommendation({...recommendation, [name]: value})
+    }
+
     return (
         <Segment clearing>
-            <Form>
-                <FormInput placeholder='Title' />
-                <FormInput placeholder='Category' />
-                <FormInput placeholder='Link' />
-                <FormInput placeholder='Date' />
-                <FormTextArea placeholder='Description' />
-                <FormInput placeholder='Country' />
-                <FormInput placeholder='City' />
-                <FormInput placeholder='Place' />
+            <Form onSubmit={handleSubmit} autoComplete='off'>
+                <FormInput placeholder='Title' value={recommendation.title} name='title'onChange={handleInputChange} />
+                <FormInput placeholder='Category' value={recommendation.category} name='category'onChange={handleInputChange} />
+                <FormInput placeholder='Link' value={recommendation.link} name='link'onChange={handleInputChange} />
+                <FormInput placeholder='Date' value={recommendation.date} name='date'onChange={handleInputChange} />
+                <FormTextArea placeholder='Description' value={recommendation.description} name='description'onChange={handleInputChange} />
+                <FormInput placeholder='Country' value={recommendation.country} name='country'onChange={handleInputChange} />
+                <FormInput placeholder='City' value={recommendation.city} name='city'onChange={handleInputChange} />
+                <FormInput placeholder='Place' value={recommendation.place} name='place'onChange={handleInputChange} />
                 <Button floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='submit' content='Cancel' />
             </Form>
