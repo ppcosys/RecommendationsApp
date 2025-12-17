a453d7913b5d668351beeee80c662fb582d32d9a
Peter <ppcosys@gmail.com>
2025-12-17 10:10:35 +0100

    refactor(layout): centralize app layout and route-based rendering

--- ReactApp/src/features/recommendations/home/HomePage.tsx
+++ ReactApp/src/features/recommendations/home/HomePage.tsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
-import { Container, Typography, Box } from '@mui/material';
+import { Container, Typography, Box, Grid } from '@mui/material';
 
 export default function HomePage() {
   return (
-    <Container maxWidth="md">
-      <Box sx={{ mt: 10 }}>
+    <Container maxWidth="lg">
+      
+      <Grid>
+        
+      </Grid>
+
+      <Box sx={{ mt: 2, bgcolor: "tomato" }}>
         <Typography variant="h3" gutterBottom>
           Home Page
         </Typography>
--- ReactApp/src/main.tsx
+++ ReactApp/src/main.tsx
@@ -12,6 +12,5 @@ createRoot(document.getElementById('root')!).render(
     <StoreContext.Provider value={store}>
       <RouterProvider router={router}/>
     </StoreContext.Provider>
-    
   </StrictMode>,
 )
